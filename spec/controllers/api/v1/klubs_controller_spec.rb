e260526b45ddcfbe967f21e72c39bac9453bf8ff
Pedro Kostelec <pedrokost@gmail.com>
2014-11-30 19:27:16 +0100

    Allow null values in JSON

--- spec/controllers/api/v1/klubs_controller_spec.rb
+++ spec/controllers/api/v1/klubs_controller_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::KlubsController, :type => :controller do
 
-	let!(:klub1) { FactoryGirl.create(:klub) } 
-	let!(:klub2) { FactoryGirl.create(:klub) } 
+	let!(:klub1) { FactoryGirl.create(:klub, latitude: 20.1, longitude: 10.1) } 
+	let!(:klub2) { FactoryGirl.create(:klub, latitude: 20.1, longitude: 10.1) } 
 	
 	describe 'GET #klubs' do
 		before do
@@ -22,9 +22,21 @@ RSpec.describe Api::V1::KlubsController, :type => :controller do
 			expect(response.status).to eq 200
 			expect(response).to match_response_schema("klubs")
 		end
-		
-		it "does not return klubs that are incomplete" do
-			# incomplete: no lat, long and name
+
+		describe "very incomplete data" do
+			before do
+				klub1.latitude = nil;
+				klub1.longitude = nil;
+				klub1.save
+
+				get :index
+			end
+
+			it "does not return klubs that are incomplete" do
+				# incomplete: no lat, long and name
+				klubs = JSON.parse(response.body)['klubs']
+				expect(klubs.length).to eq 1
+			end
 		end
 	end
 end
