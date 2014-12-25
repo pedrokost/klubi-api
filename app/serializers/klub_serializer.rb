824aa4bd9045aebc670f32a70168fdf69ae194f1
Pedro Kostelec <pedrokost@gmail.com>
2014-12-25 22:41:25 +0100

    GET /klubs includes categories key in response

--- app/serializers/klub_serializer.rb
+++ app/serializers/klub_serializer.rb
@@ -1,4 +1,4 @@
 class KlubSerializer < ActiveModel::Serializer
-  attributes :id, :name, :address, :email, :latitude, :longitude, :phone, :town, :website, :slug, :facebook_url
+  attributes :id, :name, :address, :email, :latitude, :longitude, :phone, :town, :website, :slug, :facebook_url, :categories
 
 end
