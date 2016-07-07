7cc63bb51f8bab719aa83bb536a7dc7bb302c89c
Pedro Kostelec <pedrokost@gmail.com>
2016-07-07 21:15:22 +0200

    API returns notes

--- app/serializers/klub_serializer.rb
+++ app/serializers/klub_serializer.rb
@@ -1,6 +1,6 @@
 class KlubSerializer < ActiveModel::Serializer
   cache
-  attributes :id, :name, :address, :email, :latitude, :longitude, :phone, :town, :website, :slug, :facebook_url, :categories, :parent_id
+  attributes :id, :name, :address, :email, :latitude, :longitude, :phone, :town, :website, :slug, :facebook_url, :categories, :parent_id, :notes
 
 
   def parent_id
