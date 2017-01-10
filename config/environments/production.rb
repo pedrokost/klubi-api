0ab2ec4cd0f5bd2e44c24fe17d89609d0a73a42f
Pedro Kostelec <pedrokost@gmail.com>
2017-01-10 23:19:55 +0100

    [BUG] Heroku required STDOUT logging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,8 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
+  config.logger = Logger.new(STDOUT)
+
   # Use a different cache store in production.
   config.cache_store = :dalli_store,
                       (ENV["MEMCACHIER_SERVERS"] || "").split(","),
