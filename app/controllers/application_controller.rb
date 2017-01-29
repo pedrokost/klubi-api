93e637a2700f75ca34264ec5a91d91ea2c0b9db3
Pedro Kostelec <pedrokost@gmail.com>
2017-01-29 17:26:13 +0100

    [SEO] Redirect incorrect urls to new urls (new slug format)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,9 @@ class ApplicationController < ActionController::API
 	skip_before_filter :verify_authenticity_token
 
   def index
+    new_url, changed = correct_url!
+    redirect_to new_url, status: :moved_permanently and return if changed
+
     html = bootstrap_index(params[:index_key])
     render text: html
   end
@@ -15,6 +18,50 @@ class ApplicationController < ActionController::API
   end
 
 private
+
+  def supported_categories
+    ENV['SUPPORTED_CATEGORIES'].split(',')
+  end
+
+  def correct_url!
+    # Redirects:
+    # /category/klub.slug -> /category/klub.url_slug
+    # /klub.slug -> /category/klub.url_slug
+
+    # TODO: remove this method after about 30 days, when google would
+    # hopefully already register all the 301 and update its indices
+
+    return '', false unless request.path_parameters[:path]
+
+    path_parts = request.path_parameters[:path].split('/')
+    supposed_category = path_parts.first
+
+    changed = false
+
+    if supported_categories.include? supposed_category
+      # The format is /category/slug or /category/slug/uredi
+
+      supposed_slug = path_parts[1]
+      klub = Klub.find_by(slug: supposed_slug)
+
+      if klub
+        new_url = request.original_url.sub(supposed_slug, klub.url_slug)
+        return new_url, true
+      end
+    else
+      # The format is /slug or /slug/uredi
+      supposed_slug = path_parts.first
+
+      klub = Klub.find_by(slug: supposed_slug)
+      if klub
+        category = klub.categories.first
+        new_url = request.original_url.sub(supposed_slug, "#{category}/#{klub.url_slug}")
+        return new_url, true
+      end
+    end
+
+    return '', false
+  end
 
   def bootstrap_index(index_key)
     index_key ||= 'zatresi:current-content'
---
+++ spec/requests/klub_url_spec.rb
@@ -0,0 +1,108 @@
+#!/usr/bin/ruby
+# @Author: Pedro Kostelec
+# @Date:   2017-01-29 16:22:15
+# @Last Modified by:   Pedro Kostelec
+# @Last Modified time: 2017-01-29 17:23:58
+
+require 'rails_helper'
+
+describe "Klub url redirecting" do
+  before do
+    allow(ENV).to receive(:[]).and_call_original
+    allow(ENV).to receive(:[]).with("SUPPORTED_CATEGORIES").and_return('fitnes,karate')
+  end
+
+  context "with existing klub" do
+    let!(:klub) { create(:klub, categories: ['karate'], name: 'Banana Split') }
+
+    context "new slug format" do
+      subject { get "http://www.zatresi.si/karate/banana-split-#{klub.id}" }
+
+      it "should return 200" do
+        expect(subject).to eq 200
+      end
+    end
+
+    context "old slug format" do
+      subject { get "http://www.zatresi.si/karate/banana-split?aorst=arst" }
+      it "should 301 redirect" do
+        expect(subject).to eq 301
+      end
+
+      it "should redirect to new slug format" do
+        expect(subject).to redirect_to("http://www.zatresi.si/karate/banana-split-#{klub.id}?aorst=arst")
+      end
+
+    end
+
+    context "static urls" do
+      subject { get '/oprojektu' }
+
+      it "should return 200" do
+        expect(subject).to eq 200
+      end
+    end
+
+    context "/uredi urls" do
+      subject { get 'http://www.zatresi.si/karate/banana-split/uredi' }
+
+      it "should 301 redirect" do
+        expect(subject).to eq 301
+      end
+
+      it "should redirect to new slug format" do
+        expect(subject).to redirect_to("http://www.zatresi.si/karate/banana-split-#{klub.id}/uredi")
+      end
+    end
+
+    context "unsupported category" do
+      # TODO: assert category not uspported
+
+      subject { get 'http://www.zatresi.si/lobanja/banana-split' }
+
+      it "should return 200" do
+        expect(subject).to eq 200
+      end
+    end
+
+    context "missing category with only slug" do
+      subject { get 'http://www.zatresi.si/banana-split' }
+
+      it "should 301 redirect" do
+        expect(subject).to eq 301
+      end
+
+      it "should redirect to new slug format" do
+        expect(subject).to redirect_to("http://www.zatresi.si/karate/banana-split-#{klub.id}")
+      end
+    end
+
+    context "missing category but with uredi" do
+      subject { get 'http://www.zatresi.si/banana-split/uredi' }
+
+      it "should 301 redirect" do
+        expect(subject).to eq 301
+      end
+
+      it "should redirect to new slug format" do
+        expect(subject).to redirect_to("http://www.zatresi.si/karate/banana-split-#{klub.id}/uredi")
+      end
+    end
+  end
+
+  context "with not existing klub" do
+    subject { get 'http://www.zatresi.si/this-klub-does-not-exist' }
+
+    it "should not redirect" do
+      expect(subject).to eq 200
+    end
+  end
+
+  context "root domains" do
+    subject { get '/' }
+
+    it "should not redirect" do
+      expect(subject).to eq 200
+    end
+  end
+end
